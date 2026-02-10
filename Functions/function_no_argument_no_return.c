0a8d3a17e89028cae6cbc9fb26805a9ceb349398
twinkleg07 <tg4790775@gmail.com>
2026-02-10 16:29:16 +0000

    Add function without arguments and return value
    This program demonstrates a simple function in C that does not take any arguments and does not return any value.

---
+++ Functions/function_no_argument_no_return.c
@@ -0,0 +1,14 @@
+/* 
+Program : Function without argument and without return value
+Author : Twinkle Gupta
+Date : 10/02/2026
+Description : This program demonstrates a simple function in C that does not take any arguments and does not return any value.
+*/
+#include <stdio.h>
+void greet() {
+  printf("Hello! Welcome to C Functions.\n");
+}
+int main() {
+  greet();
+  return 0;
+}
